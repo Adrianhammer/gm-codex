ee561d7c94221420289b86c132a96ebf152ad173
Adrianhmmr <adrian10hammer@gmail.com>
2025-08-09 19:23:18 +0200

    feat(Encounters): model encounter table

---
+++ gm-codex/Data/EncounterRecord.cs
@@ -0,0 +1,8 @@
+namespace gm_codex.Data;
+
+public class EncounterRecord
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+}
