847f5af604e1f3dc2ac9d8eadb9f107b0e2e987c
Adrianhmmr <adrian10hammer@gmail.com>
2025-08-14 17:55:01 +0200

    chore(cli-core): lagt til System.CommandLine og strukturert program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.CommandLine;
+using System.CommandLine.Parsing;
 using Microsoft.Extensions.Configuration;
 using Spectre.Console;
 using gm_codex.Application.Services;
 using gm_codex.Infrastructure.Data;
 using gm_codex.Infrastructure.Repositories;
 using gm_codex.Presentation.ConsoleUI;
-
-
+// --------------------
+// 1. Config & Services
+// --------------------
 var config = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
     .Build();
-        
+
 var dbConnector = new DbConnector(config);
 var entityRepository = new EntityRepository(dbConnector);
 var encounterRepository = new EncounterRepository(dbConnector);
@@ -21,12 +24,17 @@ var encounterParticipantRepository = new EncounterParticipantsRepository(dbConne
 
 var characterService = new CharacterService(entityRepository);
 
+// Ensure tables exist and render start screen
 entityRepository.CreateTable();
 encounterRepository.CreateTable();
 encounterParticipantRepository.CreateTable();
 
 ConsoleUi ui = new ConsoleUi();
 ui.RenderStartScreen();
+
+// ------------------
+// 2. Define commands
+// ------------------
 
 var commands = new Dictionary<string, Action>()
 {
