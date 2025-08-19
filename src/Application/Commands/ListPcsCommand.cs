178c96724b93555fdebfe9cef8a4ed0ca038eac4
Adrianhammer <adrian10hammer@gmail.com>
2025-08-19 14:38:43 +0200

    feat(cli): testing out new ways of creating commands

---
+++ src/Application/Commands/ListPcsCommand.cs
@@ -0,0 +1,20 @@
+﻿using Spectre.Console.Cli;
+using gm_codex.Application.Services;
+namespace gm_codex.Application.Commands;
+
+public class ListPcsCommand : Command
+{
+    //private readonly CharacterService _characterService;
+    public static CharacterService? CharacterService { get; set; }
+/*
+    public ListPcsCommand(CharacterService characterService)
+    {
+        _characterService = characterService;
+    }
+*/
+    public override int Execute(CommandContext context)
+    {
+        CharacterService?.ListPlayableCharacters();
+        return 0;
+    }
+}
