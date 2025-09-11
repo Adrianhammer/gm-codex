c6356430ad8b9f42c518f9541dd9ef87adadd296
Adrianhammer <adrian10hammer@gmail.com>
2025-09-11 22:04:19 +0200

    feat(update): add command structure for update functionality

---
+++ src/Application/Commands/UpdateNpcCommand.cs
@@ -0,0 +1,28 @@
+using gm_codex.Application.Commands.Settings;
+using gm_codex.Application.Services;
+using gm_codex.Domain.Enums;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace gm_codex.Application.Commands;
+
+public class UpdateNpcCommand : Command<UpdateSettings>
+{
+    private readonly CharacterService _characterService;
+    
+    public UpdateNpcCommand(CharacterService characterService) => _characterService = characterService;
+
+    public override int Execute(CommandContext context, UpdateSettings settings)
+    {
+        if (string.IsNullOrWhiteSpace(settings.Name))
+        {
+            AnsiConsole.MarkupLine("[red]ERROR:[/]: Name is required.");
+            return -1;
+        }
+        
+        settings.EntityType = context.Name == "npc" ? EntityType.npc : EntityType.pc;
+        
+        _characterService.UpdateEntity(settings.Name, settings.EntityType, settings.Race, settings.SubRace, settings.EntityClass, settings.SubClass, settings.MaxHp, settings.ArmorClass);
+        return 0;
+    }
+}
