0191e0b49365fba3b2bd1519cd0855a3738d9a49
Adrianhammer <adrian10hammer@gmail.com>
2025-09-17 13:06:52 +0200

    refactor(createEntity): Ensure you cannot create an entity with the same name and type

--- src/Application/Services/CharacterService.cs
+++ src/Application/Services/CharacterService.cs
@@ -15,41 +15,61 @@ public class CharacterService
     public Result<int> CreateEntity(string name, EntityType entityType, Race race, string? subRace, Class characterClass, string? subClass, string? maxHp, string? armorClass)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(name);
-        
-        var character = new Entity
-        {
-            Name = name.ToLower(),
-            Race = race,
-            EntityType = entityType,
-            SubRace = subRace?.ToLower(),
-            EntityClass = characterClass,
-            SubClass = subClass?.ToLower(),
-            MaxHp = maxHp,
-            ArmorClass = armorClass
-        };
 
         try
         {
-            var rows = _repository.InsertEntity(character);
-            if (rows == 1)
+            var existingEntity = _repository.GetEntityByName(name, entityType);
+
+            if (existingEntity is not null)
             {
-                return Result<int>.Ok(rows);
+                return Result<int>.Fail($"Entity '{name}' already exists");
             }
-            return Result<int>.Fail("Failed to insert character");
+            
+            var character = new Entity
+            {
+                Name = name.ToLower(),
+                Race = race,
+                EntityType = entityType,
+                SubRace = subRace?.ToLower(),
+                EntityClass = characterClass,
+                SubClass = subClass?.ToLower(),
+                MaxHp = maxHp,
+                ArmorClass = armorClass
+            };
+            
+            try
+            {
+                var rows = _repository.InsertEntity(character);
+                if (rows == 1)
+                {
+                    return Result<int>.Ok(rows);
+                }
+                return Result<int>.Fail("Failed to insert character");
+            }
+            
+            catch (Exception e)
+            {
+                return Result<int>.Fail(e.Message);
+            }
+            
         }
         catch (Exception e)
         {
             return Result<int>.Fail(e.Message);
         }
+        
+        
+
+        
     }
 
-    public Result<int> UpdateEntity(string name, EntityType? entityType, Race? race, string? subRace, Class? characterClass, string? subClass, string? maxHp, string? armorClass)
+    public Result<int> UpdateEntity(string name, EntityType entityType, Race? race, string? subRace, Class? characterClass, string? subClass, string? maxHp, string? armorClass)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(name);
         
         try
         {
-            var existingEntity = _repository.GetEntityByName(name, entityType!.Value);
+            var existingEntity = _repository.GetEntityByName(name, entityType);
 
             if (existingEntity is null)
             {
@@ -57,7 +77,7 @@ public class CharacterService
             }
 
             var entity = EntityMapper.ToDomain(existingEntity);
-                entity.EntityType = entityType!.Value;
+                entity.EntityType = entityType;
                 if (race is not null) entity.Race = race.Value;
                 if (subRace is not null) entity.SubRace = subRace;
                 if (characterClass is not null) entity.EntityClass = characterClass.Value;
