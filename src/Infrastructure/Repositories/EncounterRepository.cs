82082bea197bb311f557c535ea2702aaf003117f
Adrianhammer <adrian10hammer@gmail.com>
2025-09-17 15:08:57 +0200

    feat(EncounterRepo): add Insert Encounter and Get Encounter functions to repo class

--- src/Infrastructure/Repositories/EncounterRepository.cs
+++ src/Infrastructure/Repositories/EncounterRepository.cs
@@ -1,9 +1,12 @@
 using Dapper;
+using gm_codex.Application.Common;
+using gm_codex.Domain.Models;
 using gm_codex.Infrastructure.Data;
+using gm_codex.Infrastructure.Repositories.Interface;
 
 namespace gm_codex.Infrastructure.Repositories;
 
-public class EncounterRepository
+public class EncounterRepository : IEncounterRepository
 {
     private readonly DbConnector _db;
 
@@ -24,5 +27,33 @@ public class EncounterRepository
                 )";
         
         connection.Execute(query);
+    }
+
+    public int InsertEncounter(Encounter encounter)
+    {
+        using var connection = _db.CreateConnection();
+        connection.Open();
+        
+        EncounterRecord encounterRecord = new EncounterRecord
+        {
+            Name = encounter.Name,
+            Description = encounter.Description,
+        };
+        
+        var query = @"INSERT INTO Encounters (Name, Description) VALUES (@Name, @Description)";
+        
+        return connection.Execute(query, encounterRecord);
+        
+    }
+
+    public EncounterRecord? GetEncounter(string name)
+    {
+        using var connection = _db.CreateConnection();
+        connection.Open();
+        
+        var query = @"SELECT Id FROM Encounters WHERE Name = @Name";
+        
+        return connection.QuerySingleOrDefault<EncounterRecord>(query, new EncounterRecord { Name = name });
+        
     }
 }
