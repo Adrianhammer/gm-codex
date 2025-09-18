11882d00b431a805346a3d4de08864373b4aba3c
Adrianhammer <adrian10hammer@gmail.com>
2025-09-18 14:37:17 +0200

    refactor(files): Improve folder seperation for UI classes and rename ReadCommmand to ReadEntityCommand

--- src/Application/Commands/Entities/ReadEntityCommand.cs
+++ src/Application/Commands/Entities/ReadEntityCommand.cs
@@ -1,16 +1,17 @@
 using gm_codex.Application.Commands.Settings.Entities;
 using gm_codex.Application.Services;
 using gm_codex.Presentation.ConsoleUI;
+using gm_codex.Presentation.ConsoleUI.Entities;
 using Spectre.Console;
 using Spectre.Console.Cli;
 
 namespace gm_codex.Application.Commands.Entities;
 
-public class ReadCommand : Command<ReadEntitySettings>
+public class ReadEntityCommand : Command<ReadEntitySettings>
 {
     private readonly CharacterService _characterService;
 
-    public ReadCommand(CharacterService characterService) => _characterService = characterService;
+    public ReadEntityCommand(CharacterService characterService) => _characterService = characterService;
 
     public override int Execute(CommandContext context, ReadEntitySettings entitySettings)
     {
@@ -22,7 +23,6 @@ public class ReadCommand : Command<ReadEntitySettings>
         
         var result = _characterService.ReadEntity(entitySettings.Name, entitySettings.EntityType );
         ReadPcUi.ViewSingleEntity(result);
-        
         return result.Success ? 0 : -1;
     }
 }
